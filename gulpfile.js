1ad6fe74b7055317182c7861f50a068ab84cb3ad
Regis Philibert <login@regisphilibert.com>
2018-02-02 20:32:05 -0500

    add capability to minify html

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,8 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('minify-prod', function() {
-  return gulp.src('production/**/*.html')
+gulp.task('minify-html', function() {
+  return gulp.src('public/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('production'));
+    .pipe(gulp.dest('public'));
 });
-gulp.task('minify-stage', function() {
-  return gulp.src('staging/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('staging'));
-});
-
-gulp.task('sass', function () {
-  return gulp.src('./themes/regisphilibert/src/sass/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./themes/regisphilibert/static/css/'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./themes/regisphilibert/src/sass/**/*.scss', ['sass']);
-});
-
-gulp.task('default', ['minify-prod', 'minify-stage', 'sass']);
