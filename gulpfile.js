9cf8c8b18b0edadb110888b99cd7cbcc6a7b1fad
Regis Philibert <login@regisphilibert.com>
2018-08-17 13:04:52 -0400

    Removed gulp minifying script, the last touch of node js.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
-
-gulp.task('minify-html', function() {
-  return gulp.src('production/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('production'));
-});
